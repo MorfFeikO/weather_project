cbe4faa5f4f9e082a2dbce1f4817cffcd0b15de2
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2021-11-08 13:34:21 +0200

    add async response for all listed cities

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ Functions:
     pong()
         Simple fastapi test function
 """
-
 import os
-import requests
+import time
 import asyncio
+import uvicorn
 
+from aiohttp import ClientSession
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
@@ -26,7 +27,46 @@ DEFAULT_INFO = {
     'China': ('Hong Kong', 'Beijing', 'Shanghai', 'Guangzhou', 'Lanzhou'),
     'Italy': ('Rome', 'Milan', 'Florence', 'Verona', 'Venice')
 }
-# mode param to xml, to get xml format
+
+
+def get_city_url(city: str, unit: str = 'metric'):
+    """Get single city url"""
+    api = os.getenv('OPEN_WEATHER_API_SECRET')
+    return URL_PATTERN.format(city, unit, api)
+
+
+async def fetch_url_data(session, url, country, city):
+    """Get info from single city"""
+    try:
+        async with session.get(url) as response:
+            resp = await response.json()
+            city_weather = {
+                'country': country,
+                'city': city,
+                'temperature': resp['main']['temp'],
+                'description': resp['weather'][0]['description'],
+            }
+    except Exception as e:
+        print('exc - ', e)
+    else:
+        return city_weather
+
+
+@app.get('/cities')
+async def main():
+    """Get route for all cities"""
+    start = time.time()
+    tasks = []
+    async with ClientSession() as session:
+        for country in DEFAULT_INFO:
+            for city in DEFAULT_INFO[country]:
+                url = get_city_url(city)
+                task = asyncio.create_task(fetch_url_data(session, url, country, city))
+                tasks.append(task)
+        weather = await asyncio.gather(*tasks)
+    delta = time.time() - start
+    print('DELTA!!!!!!!!!!!!', delta)
+    return weather
 
 
 @app.get('/ping')
@@ -35,16 +75,5 @@ def pong():
     return {'ping': 'pong!'}
 
 
-def single_city_weather(country: str, city: str, unit: str = 'metric'):
-    api = os.getenv('OPEN_WEATHER_API_SECRET')
-    url = URL_PATTERN.format(city, unit, api)
-    response = requests.get(url)
-    if response.status_code != 200:
-        print('Error in city', city)
-    city_weather = {
-        'country': country,
-        'city': city,
-        'temperature': response.json()['main']['temp'],
-        'description': response.json()['weather'][0]['description'],
-    }
-    return city_weather
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=1111, host='127.0.0.1')
--- app/main_testing.py
+++ app/main_testing.py
@@ -1,13 +1,36 @@
+"""
+Synchron module for comparison with async
+"""
+
 import time
-from main import DEFAULT_INFO, single_city_weather
+import os
+import requests
+from main import DEFAULT_INFO, URL_PATTERN
 
 
 def find_weather():
+    """Find weather in all cities synchron"""
     weather_list = []
     for country in DEFAULT_INFO:
         for city in DEFAULT_INFO[country]:
             weather_list.append(single_city_weather(country, city))
     return weather_list
+
+
+def single_city_weather(country: str, city: str, unit: str = 'metric'):
+    """Find weather in single city synchron"""
+    api = os.getenv('OPEN_WEATHER_API_SECRET')
+    url = URL_PATTERN.format(city, unit, api)
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('Error in city', city)
+    city_weather = {
+        'country': country,
+        'city': city,
+        'temperature': response.json()['main']['temp'],
+        'description': response.json()['weather'][0]['description'],
+    }
+    return city_weather
 
 
 if __name__ == '__main__':
