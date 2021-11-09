a4fafa4d3956f93184b01fec0c2ff2ebfff73df9
Oleksandr Bozbei <oleksandr.bozbei@dxc.com>
2021-11-09 20:29:29 +0200

    add sqlalchemy models, set db & connection, add requested info to db & file

--- app/main.py
+++ app/main.py
@@ -3,17 +3,28 @@ Simple fastapi app
 
 Functions:
     pong()
-        Simple fastapi test function
+        Simple fastapi test function.
+    get_city_url(city, unit)
+        Get single city url.
+    fetch_url_data(session, url, country, city)
+        [ASYNC] Get weather info from single city
 """
+import json
 import os
-import time
 import asyncio
+import collections
+import datetime
 import uvicorn
 
 from aiohttp import ClientSession
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from sqlalchemy.exc import IntegrityError
 
+from app.models import session_sql, City, Weather
+#############################################################################
+
+#############################################################################
 
 app = FastAPI()
 load_dotenv()
@@ -22,34 +33,72 @@ load_dotenv()
 URL_PATTERN = 'http://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}'
 DEFAULT_INFO = {
     'Ukraine': ('Kyiv', 'Dnipro', 'Odesa', 'Lviv', 'Kharkiv'),
-    'United Kingdom': ('Aberdeen', 'Belfast', 'Glasgow', 'Liverpool', 'London'),
+    'UK': ('Aberdeen', 'Belfast', 'Glasgow', 'Liverpool', 'London'),
     'USA': ('New York', 'Los Angeles', 'Chicago', 'San Diego', 'Dallas'),
     'China': ('Hong Kong', 'Beijing', 'Shanghai', 'Guangzhou', 'Lanzhou'),
     'Italy': ('Rome', 'Milan', 'Florence', 'Verona', 'Venice')
 }
 
+WeatherData = collections.namedtuple("WeatherData", [
+    "country",
+    "city",
+    "temperature",
+    "condition",
+    "created_date",
+])
+
 
 def get_city_url(city: str, unit: str = 'metric'):
-    """Get single city url"""
+    """
+    Get single city url
+    ...
+    :param:
+        city: str
+            Name of the city
+        unit: str
+            URL param to choose temperature's unit of measure
+            Default:  'metric' - for temperature in Celsius
+            Optional: 'imperial' - for temperature in Fahrenheit
+                      '' - for temperature in Kelvin
+    :return:
+        url : str
+            URL for api call to openweathermap.org
+    """
     api = os.getenv('OPEN_WEATHER_API_SECRET')
     return URL_PATTERN.format(city, unit, api)
 
 
 async def fetch_url_data(session, url, country, city):
-    """Get info from single city"""
+    """
+    [ASYNC] Get weather info from single city
+    ...
+    :param:
+        session: ClientSession()
+            async ClientSession obj
+        url: str
+            URL for api call
+        country: str
+            Name of country.
+        city: str
+            Name of city.
+
+    :return:
+        WeatherData: namedtuple
+            Namedtuple object with fetched weather data.
+    """
     async with session.get(url) as response:
         resp = await response.json()
-        city_weather = {
-            'country': country,
-            'city': city,
-            'temperature': resp['main']['temp'],
-            'description': resp['weather'][0]['description'],
-        }
-    return city_weather
+    return WeatherData(
+        country,
+        city,
+        resp['main']['temp'],
+        resp['weather'][0]['description'],
+        (lambda: datetime.datetime.utcnow() if country in ('Ukraine', 'UK', ) else datetime.datetime.utcnow().strftime(
+            '%Y%m%d'))()
+         )
 
 
-@app.get('/cities')
-async def main():
+async def gather_weather():
     """Get route for all cities"""
     tasks = []
     async with ClientSession() as session:
@@ -61,6 +110,50 @@ async def main():
                 tasks.append(task)
         weather = await asyncio.gather(*tasks)
     return weather
+
+
+@app.get('/cities')
+async def replace_with_rabbitmq():
+    weather_data_list = await gather_weather()
+
+    for weather_data in weather_data_list:
+        if weather_data.country in ('Ukraine', 'UK', ):
+            save_data_to_db(weather_data)
+        else:
+            save_data_to_file(weather_data)
+
+
+def save_data_to_db(data):
+    save_city_to_db(data)
+    save_weather_to_db(data)
+
+
+def save_city_to_db(data):
+    try:
+        session_sql.add(City(
+            name=data.city,
+            country=data.country,
+        ))
+        session_sql.commit()
+    except IntegrityError:
+        session_sql.rollback()
+
+
+def save_weather_to_db(data):
+    city = session_sql.query(City).filter(City.name == data.city).one()
+    session_sql.add(Weather(
+        city_id=city.id,
+        temperature=data.temperature,
+        condition=data.condition,
+        created_date=data.created_date,
+    ))
+    session_sql.commit()
+
+
+def save_data_to_file(data):
+    filename = f"{data.country}_{data.city}_{data.created_date}.txt"
+    with open('./data/' + filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data._asdict(), json_file)
 
 
 @app.get('/ping')
---
+++ app/models.py
@@ -0,0 +1,76 @@
+"""
+SQLAlchemy database models module.
+
+Objects:
+    session_sql:
+        Bound session object.
+
+Classes:
+    Weather:
+        Database weather model.
+    City:
+        Database city model.
+"""
+import os
+
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.orm import Session, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+load_dotenv()
+
+engine = create_engine("postgresql+pg8000://{}:{}@{}/weatherdb".format(
+    os.getenv('DB_USERNAME'),
+    os.getenv('DB_PASSWORD'),
+    os.getenv('DB_HOST_PORT'),
+))
+conn = engine.connect()
+session_sql = Session(bind=engine)
+base = declarative_base()
+
+
+class Weather(base):
+    """
+    Database weather model.
+    ...
+    :param:
+        id: Integer, pk
+        city_id: ForeignKey("City.id")
+        temperature: Float
+        condition: String(100)
+        created_date: DateTime()
+    """
+    __tablename__ = "weather"
+
+    id = Column("id", Integer, primary_key=True)
+    city_id = Column("city_id", ForeignKey("city.id"))
+    temperature = Column("temperature", Float)
+    condition = Column("condition", String(100))
+    created_date = Column("created_date", DateTime())
+
+
+class City(base):
+    """
+    Database city model.
+    ...
+    :param:
+        id: Integer, pk
+        name: String(85)
+        country: String(56)
+        UniqueConstraint(name, country)
+    """
+    __tablename__ = "city"
+
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(85))
+    country = Column("country", String(56))
+    weather = relationship("Weather", cascade="all,delete", backref="city")
+
+    __table_args__ = (UniqueConstraint("name", "country", name="location"), )
+
+
+base.metadata.create_all(engine)
